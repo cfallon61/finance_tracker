5b1295911b6819e7085bb63d521d302ff804f644
cfallon61 <fallon2@purdue.edu>
2019-04-16 17:17:21 -0400

    added example login page for testing, using the express package now instead of http

--- server.js
+++ server.js
@@ -1,47 +1,58 @@
-let http   = require('http');
 let mysql  = require("mysql");
-let fs     = require("fs");
+let bcrypt = require("bcrypt");
+let express = require("express");
+let path    = require("path");
+let sessions = require("client-sessions");
+
+// config.json holds the credentials for the database and other important
+// init settings
 const init = require("./config.json");
 
+var app = express();
 
-const table_fields = [
-    'FIRST',
-    'LAST',
-    'EMAIL',
-    'PASS',
-];
+// root directory
+const root = path.join(__dirname, "./root");
 
-const db = mysql.createConnection(init);
+const db = mysql.createConnection(init.db);
 
 // connect to the database
 db.connect((err) => {
     if (err) throw err;
     console.log("Connected to database");
-});
-
-var values = ['Steve', '46'];
-db.query("INSERT INTO test (name, age) VALUES ('Steve', '46')", function(err, result)
-{
-    console.log("inserting test info into db");
-    if (err) throw err;
-    console.log(result);
-});
-
-
-const server = http.createServer(function(request, response)
-{
-    // console.log(init);
-    db.query("SELECT * FROM test;", function(err, result)
+    // just to verify that it connected to the DB, print to console
+    db.query("SHOW TABLES", function(err, result)
     {
-       if (err) throw err;
-       console.log(result);
-    });
+        if (err) throw err;
+        console.log(result);
+    })
+});
 
-    response.writeHead(200, "OK", "Content-type: text/plain");
-    response.write("Place holder login page");
-    response.end();
-}).listen(8080);
+// set the application to use the cookies specified in config.json
+app.use(sessions(init.cookies));
+app.use(express.static(root));
 
+// start the server listening on port 8080
+app.listen(8080, function(err)
+{
+    if (err) throw err;
+    console.log("Server started");
+});
 
-// server.on("connect", );
+// Get root file path
+app.get('/', function(request, result)
+{
+    console.log("GET / " + request.headers + "\n");
+    result.sendFile(path.join(root, "login.html"));
+});
+
+function login()
+{
+
+}
+
+function onSubmit()
+{
+    document.getElementById('login');
+    console.log("button clicked");
+}
 
