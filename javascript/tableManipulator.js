705b4a151936971e00e2b5fd6595c9a281faeb8c
JesseGallardo <gallardotjesse@gmail.com>
2019-04-17 17:08:42 -0400

    Added Remove Button and Column

--- javascript/tableManipulator.js
+++ javascript/tableManipulator.js
@@ -14,9 +14,16 @@ function insertTableRow() {
     var dateCell = row.insertCell(1);
     var amountCell = row.insertCell(2);
     var typeCell = row.insertCell(3);
+    var removeCell = row.insertCell(4);
 
     nameCell.innerHTML = document.getElementById("nameField").value;
     dateCell.innerHTML = convertMonthToNumber(tableMonth) + "/" + document.getElementById("dayField").value;
     amountCell.innerHTML = "$" + document.getElementById("amountField").value;
     typeCell.innerHTML = document.getElementById("typeDropdown").value;
+    removeCell.innerHTML = "<button type=\"button\" onclick=\"deleteTableRow()\">Delete</button>";
+}
+
+function deleteTableRow(x) {
+    var rowIndex = x.parentNode.parentNode;
+    rowIndex.parentNode.removeChild(rowIndex);
 }
