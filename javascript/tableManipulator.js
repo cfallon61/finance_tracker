f9adebdf525f5731762fe05676843b69d4005d19
JesseGallardo <gallardotjesse@gmail.com>
2019-04-17 13:25:24 -0400

    Added Debit/Credit Option and background colors

--- javascript/tableManipulator.js
+++ javascript/tableManipulator.js
@@ -13,8 +13,10 @@ function insertTableRow() {
     var nameCell = row.insertCell(0);
     var dateCell = row.insertCell(1);
     var amountCell = row.insertCell(2);
+    var typeCell = row.insertCell(3);
 
     nameCell.innerHTML = document.getElementById("nameField").value;
     dateCell.innerHTML = convertMonthToNumber(tableMonth) + "/" + document.getElementById("dayField").value;
     amountCell.innerHTML = "$" + document.getElementById("amountField").value;
+    typeCell.innerHTML = document.getElementById("typeDropdown").value;
 }
