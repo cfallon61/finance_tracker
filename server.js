398aaef8f4573ca880be7c10a2cdedac2bbe31ed
cfallon61 <fallon2@purdue.edu>
2019-04-22 20:53:24 -0400

    "fixed authentication and redirection issues" he said

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ app.use(express.static(root));
 app.use((req, res, next) =>
 {
   if (req.cookies.tracker && !req.session.uid)
+  {
     res.clearCookie(init.cookies.key);
+  }
   next();
 });
 
@@ -62,7 +64,7 @@ app.get("/login", is_logged_in, (req, res) =>
   console.log("\n\nGET /login");
   console.log(req.cookies);
   console.log(req.session);
-  res.sendFile(path.join(root,"login.html"));
+  res.sendFile(path.join(root, "login.html"));
 });
 
 // user has submitted the login information
@@ -79,43 +81,45 @@ app.post('/login', is_logged_in, (request, response) =>
   // empty username or password
   if (!email || !password)
   {
-    response.setHeader("Error", "Please fill all fields.");
-    response.sendStatus(401);
+    response.status(401).send("Error: Please fill all fields.");
     return;
   }
+
   // query db for user info
-  db.query("SELECT * FROM ?? WHERE EMAIL=?",  [init.db.user_table, email], (err, res) =>
+  db.query("SELECT * FROM ?? WHERE EMAIL=?", [init.db.user_table, email], (err, res) =>
   {
-    if (err)
-    {
-     console.log(err);
-     console.log(err.code);
-    }
+    if (err) console.log("Code: ", err.code, "SQL: ", err.sql);
     console.log(res);
 
     // if the response is not empty
     if (res !== undefined && res.length > 0)
     {
       // compare the password to the hashed password
-      if (bcrypt.compareSync(password, res[0].PASSHASH) === false)
+      if (res[0].PASSHASH === null)
+      {
+        response.status(401).send("Error: Invalid username or password.");
+        return;
+      }
+      if (bcrypt.compareSync(password, res[0].PASSHASH))
       {
         console.log("Password does not match");
-        response.setHeader("Error", "Invalid username or password");
-        response.sendStatus(401);
+        response.status(401).send("Error: Invalid username or password.");
+
       }
       else
       {
-        console.log("redirecting to user dashboard");
+        console.log("\nredirecting to user dashboard");
         request.session.uid = email;
-        console.log("userid: ", request.session.uid);
-        response.redirect('/dashboard');
+        console.log("userid:", request.session.uid);
+        const url = `/dashboard/${request.session.uid}`;
+        console.log(url);
+        response.status(202).send(url);
       }
     }
     else
     {
       console.log("User doesn't exist");
-      response.setHeader("Error", "No such user");
-      response.sendStatus(401);
+      response.status(401).send("Error: No such user.");
     }
   });
 });
@@ -123,11 +127,11 @@ app.post('/login', is_logged_in, (request, response) =>
 app.get("/signup", is_logged_in, (req, res) =>
 {
   console.log("\n\nGET /signup\n");
-  res.sendFile(path.join(root,"signup.html"));
+  res.sendFile(path.join(root, "signup.html"));
 });
 
 // user has submitted the login information
-app.post('/signup', is_logged_in, (request, response) =>
+app.post('/signup', (request, response) =>
 {
   console.log("\n\nPOST /signup ");
   console.log(request.headers);
@@ -139,105 +143,113 @@ app.post('/signup', is_logged_in, (request, response) =>
   if (!email || !password || !name)
   {
     console.log("invalid password, email, or name");
-    response.setHeader("Error", "Invalid Email, Name, or Password");
-    response.status(400);
-    return false;
+    response.status(400).send("Error: Invalid Email, Name, or Password");
+    return;
   }
-  signup(email, name, password)
-    .then((err) =>
+  var passhash = bcrypt.hashSync(password, 10);
+
+  const data = [email, name, passhash];
+  check_user_in_users(data)
+    .then(create_user_table)
+    .then(insert_to_users_table)
+    .then(() =>
     {
       request.session.uid = email;
-      response.redirect("/dashboard");
-    }).catch(err =>
+      const url = `/dashboard/${request.session.uid}`;
+      response.status(202).send(url);
+    })
+    .catch(err => response.status(500).send({"Error":err}));
+});
+
+// the user's dashboard
+app.get('/dashboard/:uid', not_logged_in, (request, response) =>
+{
+  console.log("\nGET /dashboard/:", request.session.uid);
+  const uid = request.session.uid;
+  const query_string = "SELECT * FROM " + mysql.escapeId(uid, true);
+
+  new Promise((resolve, reject) =>
+  {
+    db.query(query_string, uid, (err, res) =>
     {
-      response.status(401).send(err);
+      if (err) reject(err);
+      else resolve(res);
+    });
+  }).then((res) =>
+    {
+      // TODO change this placeholder
+      console.log("redirecting");
+      response.redirect("/");
+    })
+    .catch((err) =>
+    {
+      console.log("Code: ", err.code, "SQL: ", err.sql);
+      response.status(501).send("Error", "The server encountered an error.");
     });
 });
 
-function signup(email, name, password)
+// query the database during creation to see if the user already exists in the table of users
+function check_user_in_users(data)
 {
+  const email = data[0];
   return new Promise((resolve, reject) =>
   {
-    bcrypt.hash(password, 10, (err, hash) =>
+    db.query("SELECT * FROM ?? WHERE EMAIL=?", [init.db.user_table, email], (err, res) =>
     {
-      if (err) reject(err);
-      check_user_in_users(email)
-        .then(create_user_table(email))
-        .then(insert_to_users_table(email, name, hash))
-        .catch(err => console.log(err));
-    });
-  });
-}
+      if (err) reject([err.code, err.sql]);
 
-// query the database during creation to see if the user already exists in the table of users
-function check_user_in_users(email)
-{
-  return new Promise((reject, resolve) =>
-  {
-    db.query("SELECT * FROM ?? WHERE EMAIL=?", [init.db.user_table, email], (err, res) => {
-      if (err) reject(err);
       console.log(res);
-      if (res !== undefined && res.length > 0) reject("User already exists.");
-      else resolve();
+
+      if (res.length > 0) reject("User already exists.");
+      else resolve(data);
     });
   });
 }
 
 // create a new table for the user
-function create_user_table(email) {
-  return new Promise((resolve, reject) => {
+function create_user_table(data)
+{
+  const email = data[0];
+  return new Promise((resolve, reject) =>
+  {
     var query = "CREATE TABLE " + mysql.escapeId(email, true) + " LIKE template";
-    db.query(query, (err, res) => {
+    db.query(query, (err, res) =>
+    {
       if (err)
       {
-        console.log(err);
+        console.log("Code: ", err.code, "Sql: ", err.sql);
         reject("User already exists.");
       }
       else
       {
         console.log(email, " table created");
-        resolve(res);
+        resolve(data);
       }
     });
   });
 }
 
 // add the user to the users table
-function insert_to_users_table(email, name, passhash)
+function insert_to_users_table(data)
 {
+  const email = data[0];
+  const name = data[1];
+  const hash = data[2];
+
   return new Promise((resolve, reject) =>
   {
-    var data = {USERNAME: name, PASSHASH: passhash, EMAIL: email};
+    var data = {USERNAME: name, PASSHASH: hash, EMAIL: email};
     db.query("INSERT INTO ?? SET ?", [init.db.user_table, data], (err, res) =>
     {
-      if (err) reject(err);
+      if (err) reject([err.code, err.sql]);
       else
       {
-        console.log(email, " inserted into table of users.");
-        resolve(res);
+        console.log(email, " inserted into table of users.", "hash: ", hash);
+        resolve(data);
       }
     });
   });
 }
-// the user's dashboard
-app.get('/dashboard', not_logged_in, (request, response) =>
-{
-  var uid = request.session.uid;
-  var query_string = "SELECT * FROM " + mysql.escapeId(uid, true);
-  db. query(query_string, uid, function (err, res)
-  {
-    if (err)
-    {
-      console.log(err);
-      response.status(501);
-    }
-    console.log(res);
-    if (res)
-    {
-     response.sendFile(path.join(root,"dashboard/dashboard.html"));
-    }
-  });
-});
 
 // check if the user is logged into an account
 function is_logged_in(request, response, next)
@@ -247,7 +259,7 @@ function is_logged_in(request, response, next)
   if (request.session.uid && request.cookies.tracker)
   {
     console.log("redirecting to user dashboard");
-    response.redirect("/dashboard");
+    response.redirect(`/dashboard/${request.session.uid}`);
   }
   // do the next function
   else
@@ -268,7 +280,7 @@ function not_logged_in(request, response, next)
   // do the next function
   else
   {
-    console.log("user logged in");
+    console.log("user logged in: ", request.session.uid);
     next();
   }
 }
